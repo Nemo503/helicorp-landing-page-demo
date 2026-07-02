bd2e794be8a61b9c76509c22320802a15e719fe1
Nemo <lanh050304@gmail.com>
2026-07-03 04:03:17 +0700

    fix: remove invalid devIndicators config

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-  },
   allowedDevOrigins: ['192.168.100.200'],
 };
 
