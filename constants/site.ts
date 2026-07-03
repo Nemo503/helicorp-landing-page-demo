c09c07faa61237348fc834b13c390053df457f17
Nemo <lanh050304@gmail.com>
2026-07-03 16:21:24 +0700

    feat: add Zalo and Youtube icons back to footer

--- constants/site.ts
+++ constants/site.ts
@@ -6,8 +6,9 @@
 
 import {
   Globe,
+  MessageCircle,
 } from 'lucide-react';
-import { FacebookIcon, InstagramIcon } from '@/components/ui/SocialIcons';
+import { FacebookIcon, InstagramIcon, YoutubeIcon } from '@/components/ui/SocialIcons';
 import type { NavItem, SocialLink, FooterLinkGroup } from '@/types';
 
 /* --------------------------------
@@ -57,7 +58,9 @@ export const HERO_CONTENT = {
 export const SOCIAL_LINKS: SocialLink[] = [
   { icon: FacebookIcon as any, href: 'https://www.facebook.com/helipet.vn', label: 'Facebook' },
   { icon: InstagramIcon as any, href: 'https://www.instagram.com/helipet.vn/', label: 'Instagram' },
-  { icon: Globe, href: 'https://helipet.vn/', label: 'Website' },
+  { icon: YoutubeIcon as any, href: 'https://youtube.com/@helipet', label: 'YouTube' },
+  { icon: MessageCircle as any, href: 'https://zalo.me/helipet', label: 'Zalo' },
+  { icon: Globe as any, href: 'https://helipet.vn/', label: 'Website' },
 ];
 
 /* --------------------------------
